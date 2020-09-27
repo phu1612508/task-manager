67557d5c027e12cea3025d0cc1aac38ac2786043
phu1612508 <43996827+phu1612508@users.noreply.github.com>
2020-09-27 08:29:38 +0700

    Separate Route Files

---
+++ src/routers/user.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const User = require("../models/user");
+const router = new express.Router();
+
+router.post("/users", async (req, res) => {
+    const user = new User(req.body);
+    try {
+        await user.save();
+        res.status(201).send(user);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+router.get("/users", async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+router.get("/users/:id", async (req, res) => {
+    const _id = req.params.id;
+    try {
+        const user = await User.findById(_id);
+        if (!user) {
+            return res.status(404).send("User not found");
+        }
+        res.send(user);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+router.patch("/users/:id", async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ["name", "password", "email", "age"];
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update);
+    });
+    if (!isValidOperation) {
+        return res.status(400).send({
+            error: "Invalid updates",
+        });
+    }
+    try {
+        const _id = req.params.id;
+        const user = await User.findByIdAndUpdate(_id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+router.delete("/users/:id", async (req, res) => {
+    const _id = req.params.id;
+    try {
+        const user = await User.findByIdAndDelete(_id);
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+module.exports = router
