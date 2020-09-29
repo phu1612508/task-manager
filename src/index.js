e68dad08bf86e218883bc0171c3a3cecae330cac
phu1612508 <43996827+phu1612508@users.noreply.github.com>
2020-09-29 10:21:47 +0700

    Accept AuthToken

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 require("./db/mongoose.js");
-const User = require("./models/user");
-const Task = require("./models/task");
 const userRouter = require("./routers/user");
-const taskRouter=require("./routers/task");
+const taskRouter = require("./routers/task");
 
 const app = express();
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
